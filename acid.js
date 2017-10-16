935cb82e7188459e32eb44c7b07582d5012dcb64
Matt Butcher <matt.butcher@microsoft.com>
2017-10-16 12:00:34 -0600

    Fixed for draft

--- acid.js
+++ acid.js
@@ -1,12 +1,25 @@
 const {events, Job} = require("libacid")
 
 events.on("imagePush", (e, p) => {
-  var name = "helm-hello"
-  // run a Helm build
-  helm := new Job("helm", "lachlanevenson/k8s-helm:latest")
+  var name = "example-hello"
+  var docker = JSON.parse(e.payload)
+  var version = docker.push_data.tag || "dev"
+
+  var helm = new Job("helm", "lachlanevenson/k8s-helm:latest")
   helm.tasks = [
-    "helm upgrade --install " + name + " charts/helm-hello"
+    "helm upgrade --set tag=" + version + " --install " + name + " /src/charts/helm-hello"
   ]
 
-  helm.run()
+  var slack = new Job("slack-notify", "technosophos/slack-notify:latest", ["/slack-notify"])
+
+  helm.run().then( result => {
+    slack.env = {
+      SLACK_WEBHOOK: project.secrets.SLACK_WEBHOOK,
+      SLACK_USERNAME: "AcidBot",
+      SLACK_TITLE: ":helm: upgraded " + name,
+      SLACK_MESSAGE: result.toString() + " <https://" + project.repo.name + ">",
+      SLACK_COLOR: "#0000ff"
+    }
+    slack.run()
+  })
 })
