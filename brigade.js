1ff00abf1fad202c8c25deccdccaf485910e2428
Matt Butcher <matt.butcher@microsoft.com>
2017-10-16 13:08:32 -0600

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,11 +1,14 @@
 const {events, Job} = require("libacid")
 
+// Set to 2.5.1 b/c of ACS requirements
+const helmTag = "v2.5.1"
+
 events.on("imagePush", (e, p) => {
   var name = "example-hello"
   var docker = JSON.parse(e.payload)
   var version = docker.push_data.tag || "dev"
 
-  var helm = new Job("helm", "lachlanevenson/k8s-helm:latest")
+  var helm = new Job("helm", "lachlanevenson/k8s-helm:" + helmTag)
   helm.tasks = [
     "helm upgrade --set tag=" + version + " --install " + name + " /src/charts/helm-hello"
   ]
@@ -17,7 +20,7 @@ events.on("imagePush", (e, p) => {
       SLACK_WEBHOOK: project.secrets.SLACK_WEBHOOK,
       SLACK_USERNAME: "AcidBot",
       SLACK_TITLE: ":helm: upgraded " + name,
-      SLACK_MESSAGE: result.toString() + " <https://" + project.repo.name + ">",
+      SLACK_MESSAGE: result.toString(),
       SLACK_COLOR: "#0000ff"
     }
     slack.run()
